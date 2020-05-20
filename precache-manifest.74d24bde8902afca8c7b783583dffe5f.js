9866e5cb4bba0b01239cdb2fc1bc88c165d22fc9
Frederico Estrela <f.estrelagaiva@elsevier.com>
2020-05-19 21:32:56 -0300

    Updates

---
+++ precache-manifest.74d24bde8902afca8c7b783583dffe5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff73182514a395253f53d8229254b704",
+    "url": "/brain-juice/index.html"
+  },
+  {
+    "revision": "61b5e96e61157fad9e84",
+    "url": "/brain-juice/static/js/2.774b3bdf.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/brain-juice/static/js/2.774b3bdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f51fb8390d6dd64cbd3",
+    "url": "/brain-juice/static/js/main.db55a43c.chunk.js"
+  },
+  {
+    "revision": "4f784237321b8df019b0",
+    "url": "/brain-juice/static/js/runtime-main.08922942.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b0030be940f2b61d2875abdaef870145.js"
+  "/brain-juice/precache-manifest.74d24bde8902afca8c7b783583dffe5f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/brain-juice/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
