56a9dc938a1b635ccd66d9d6faab46fe27b0aeb5
Frederico Estrela <f.estrelagaiva@elsevier.com>
2020-05-19 21:48:33 -0300

    Preparing heroku

--- packages/frontend/src/helpers/api.ts
+++ packages/frontend/src/helpers/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:9000';
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:9000' : '';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export default axios;
