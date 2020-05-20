7ae85056014e878c16b8fcfa86623389c72e7759
Frederico Estrela <f.estrelagaiva@elsevier.com>
2020-05-19 21:53:44 -0300

    Heroku

--- packages/frontend/src/helpers/api.ts
+++ packages/frontend/src/helpers/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:9000' : '';
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:9000' : 'https://mind-juice.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export default axios;
